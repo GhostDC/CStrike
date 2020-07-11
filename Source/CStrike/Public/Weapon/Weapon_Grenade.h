732debd4791241a1763bdaa0a7cc1d1abb49eda8
GhostDC <q1007199608@hotmail.com>
2020-07-11 17:00:39 +0800

    Initial weapon_grenade class

---
+++ Source/CStrike/Public/Weapon/Weapon_Grenade.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Weapon/Weapon_Base.h"
+#include "Weapon_Grenade.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CSTRIKE_API AWeapon_Grenade : public AWeapon_Base
+{
+	GENERATED_BODY()
+	
+};
